2f0d4aef5a812a25d6aeca6b70475fd759e5bfdd
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-01-26 21:32:38 -0700

    Add project files.

---
+++ Mission4/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieInputContext _blahContext {get; set;}
+
+        public HomeController(ILogger<HomeController> logger, MovieInputContext someName)
+        {
+            _logger = logger;
+            _blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(MovieInput ar)
+        {
+            _blahContext.Add(ar);
+            _blahContext.SaveChanges();
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        //public IActionResult Movies()
+        //{
+        //    return View();
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission4/Models/MovieInputContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class MovieInputContext : DbContext
+    {
+        public MovieInputContext (DbContextOptions<MovieInputContext>options): base (options)
+        {
+         
+        }
+
+        public DbSet<MovieInput> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieInput>().HasData(
+
+                new MovieInput
+                {
+                    MovieID = 1,
+                    Category = "Action/Adventure",
+                    Title ="Hary Potter and the Sorcerers Stone",
+                    Year = 2001,
+                    Director = "Chris Columbus",
+                    Rating =  "PG",
+                    Edited = false,
+                    Notes = "Awesome movie!",
+                },
+                new MovieInput
+                {
+                    MovieID = 2,
+                    Category = "Romance/Drama",
+                    Title = "Little Women",
+                    Year = 2019,
+                    Director = "Greta Gerwig",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "Sav",
+                    Notes = "Loved watching it! ",
+                },
+                new MovieInput
+                {
+                    MovieID = 3,
+                    Category = "Romance/Comedy",
+                    Title = "You Again",
+                    Year = 2010,
+                    Director = "Andy Fickman",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "Lexi",
+                    Notes = "Love Betty White! ",
+                }
+                ) ;
+        }
+    }
+}
